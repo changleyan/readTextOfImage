c110766e359514aeb512c95f820b3909cddbbaf2
Chang <lchang@defontana.com>
2024-08-30 16:55:24 -0400

    funciona 1

---
+++ main.py
@@ -0,0 +1,29 @@
+from PIL import Image
+import pytesseract
+import re
+
+# Configura el path de tesseract si no está en tu PATH
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'  # Ajusta según tu instalación
+
+# Cargar la imagen
+img = Image.open('max.jpg')
+
+# Extraer texto de la imagen
+texto = pytesseract.image_to_string(img)
+
+# Imprimir el texto extraído
+print("Texto extraído:")
+print(texto)
+
+# Buscar ocurrencias de la palabra y extraer números
+palabra_buscar = 'Pallet '  # Cambia esto por la palabra que buscas
+patron = rf'{palabra_buscar}\D*(\d+)'  # Busca la palabra seguida de números
+
+numeros = re.findall(patron, texto)
+
+# Convertir los números encontrados a enteros
+numeros = list(map(int, numeros))
+
+# Imprimir los números extraídos
+print("Números extraídos:")
+print(numeros)
